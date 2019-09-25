ac85264f4f7b394685c5b2fe12aa854778dcc427
Joseph Miner <herrminer@gmail.com>
2019-09-25 14:41:38 -0500

    slightly more lenient on organization.csv formatting

--- src/main/groovy/com/herrminer/metrics/service/OrganizationService.groovy
+++ src/main/groovy/com/herrminer/metrics/service/OrganizationService.groovy
@@ -4,8 +4,12 @@ import com.herrminer.metrics.model.Organization
 import com.herrminer.metrics.model.Role
 import com.herrminer.metrics.model.Team
 import com.herrminer.metrics.model.User
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class OrganizationService {
+
+  private static final Logger logger = LoggerFactory.getLogger(OrganizationService)
 
   private static Organization organization
 
@@ -23,14 +27,19 @@ class OrganizationService {
     List<User> users = new ArrayList<>()
     new File(getClass().getResource("/organization.csv").toURI()).eachLine { line, num ->
 
-      if (num > 1) {
+      if (num > 1 && line.length()) {
         String[] parts = line.split(',')
 
+        if (parts.length != 5) {
+          logger.error "line has incorrect number of fields: ${line}\n"
+          return
+        }
+
         def user = new User(
-          firstName: parts[0],
-          lastName: parts[1],
-          userName: parts[2],
-          role: Role.valueOf(parts[4])
+          firstName: parts[0].trim(),
+          lastName: parts[1].trim(),
+          userName: parts[2].trim(),
+          role: getRole(parts[4].trim())
         )
 
         def teamName = parts[3]
@@ -50,6 +59,15 @@ class OrganizationService {
     }
 
     organization
+  }
+
+  static Role getRole(String roleName) {
+    try {
+      return Role.valueOf(roleName)
+    } catch (e) {
+      logger.error "Invalid role name: ${roleName}, must be one of ${Role.values()}"
+      System.exit(1) // hard stop to prevent downstream errors due to non-existent role
+    }
   }
 
 }
