5ed5bf12a298ec5f8faed77190f74f1fccd030e7
Joseph Miner <herrminer@gmail.com>
2021-08-14 19:07:14 -0500

    adding user URL

--- src/main/groovy/com/herrminer/metrics/service/reports/UsersReport.groovy
+++ src/main/groovy/com/herrminer/metrics/service/reports/UsersReport.groovy
@@ -14,9 +14,9 @@ class UsersReport extends MetricReport {
   @Override
   File buildCsvFile() {
     def outputFile = createReportFile('users')
-    outputFile << 'team,login,name\n'
+    outputFile << 'team,login,name,url\n'
     reportingContext.organization.users.each { id, user ->
-      outputFile << "${user.githubTeam.name},${user.login},${user.name ?: user.login}\n"
+      outputFile << "${user.githubTeam.name},${user.login},${user.name ?: user.login},${user.url}\n"
     }
     outputFile
   }
